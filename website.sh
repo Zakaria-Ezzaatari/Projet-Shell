1e3617a0963246908a31a918a9684ecf0ade49f2
Zakaria Ezzaatari <zakaria.ezzaatari@laposte.net>
2021-12-07 21:16:28 +0100

    Added the ability to see messages and created a JSON file to contain them

--- website.sh
+++ website.sh
@@ -37,7 +37,7 @@ menu()
 
 check_permission()
 {
-    if [[ -x website.sh ]]; then
+    if [[ -w website.sh ]]; then
     echo "This script has the permission to write"
     else
         echo "This script doesn't have the permission to write" 1>&2
@@ -83,9 +83,11 @@ load_images()
     else
         echo -e "<div class='carousel-item'>" >> websitetest.html
     fi
-    echo -e "<img class='d-block w-100' src='$file' alt='First slide'>
+    echo -e "<img class='d-block mx-auto' src='$file' alt='First slide'>
     </div>" >> websitetest.html
 }
+
+
 
 build()
 {
@@ -105,12 +107,13 @@ build()
     <!-- Bootstrap CSS -->
     <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm' crossorigin='anonymous'>
 
+    
     <title>Blog</title>
     </head>
     <body>
     
     <div id='carouselExampleControls' class='carousel slide' data-ride='carousel'>
-    <div class='carousel-inner'>
+    <div class='carousel-inner' role='listbox' style='max-width:900px; max-height:600px !important;'>
     " >> websitetest.html
 
 
@@ -138,16 +141,52 @@ build()
   </a>
 </div>" >> websitetest.html
 
+
+#install required for handling JSON files efficiently
+#sudo apt-get install jq
+
+touch messages.json
+echo -e '[
+    {
+        "user": "test",
+        "message": "Sample Text.exe"
+    },
+    {
+        "user": "lucas",
+        "message": "lorem"
+    }
+    ]' > messages.json
+
 echo -e "
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src='https://code.jquery.com/jquery-3.2.1.slim.min.js' integrity='sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN' crossorigin='anonymous'></script>
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js' integrity='sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q' crossorigin='anonymous'></script>
-    <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js' integrity='sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl' crossorigin='anonymous'></script>
-  </body>
-</html>" >> websitetest.html
+<table class='table table-bordered'>
+  <thead>
+    <tr>
+      <th scope='col'>User</th>
+      <th scope='col'>Message</th>
+      </tr>
+  </thead>
+  <tbody>
+" >> websitetest.html
 
+# read each item in the JSON array to an item in the Bash array
+readarray -t my_array < <(jq -c '.[]' messages.json)
 
+# iterate through the Bash array
+for item in "${my_array[@]}"; do
+    user=$(jq '.user' <<< "$item")
+    message=$(jq '.message' <<< "$item")
+    echo -e "
+    <tr>
+      <td>$user</td>
+      <td>$message</td>
+    </tr>
+    " >> websitetest.html
+done
+
+echo -e "
+</tbody>
+</table>
+" >> websitetest.html
 
 
 
