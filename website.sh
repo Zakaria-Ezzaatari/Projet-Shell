3d4059ae6fd6945c902ed9f0a218f6d5eeb8e8a4
Zakaria Ezzaatari <zakaria.ezzaatari@laposte.net>
2021-12-07 19:33:05 +0100

    Finished a first version of the builing function

--- website.sh
+++ website.sh
@@ -73,12 +73,28 @@ check_images()
 
 }
 
+load_images()
+{
+    local file=$1
+    local first=$2
+
+    if [[ $2 == true ]]; then
+        echo -e "<div class='carousel-item active'>" >> websitetest.html
+    else
+        echo -e "<div class='carousel-item'>" >> websitetest.html
+    fi
+    echo -e "<img class='d-block w-100' src='$file' alt='First slide'>
+    </div>" >> websitetest.html
+}
+
 build()
 {
+    local first=true
+    
     check_connection
     check_permission
     check_images
-    echo > websitetest.php
+    echo > websitetest.html
     echo -e "<!doctype html>
     <html lang='en'>
     <head>
@@ -93,7 +109,49 @@ build()
     </head>
     <body>
     
-    " >> websitetest.php
+    <div id='carouselExampleControls' class='carousel slide' data-ride='carousel'>
+    <div class='carousel-inner'>
+    " >> websitetest.html
+
+
+    for file in ./images/*;
+    do
+        case $file in
+            (*.jpeg) 
+            load_images "$file" "$first";;
+            (*.jpg) 
+            load_images "$file" "$first" ;;
+            (*.png) 
+            load_images "$file" "$first" ;;
+        esac  
+        first=false
+    done;
+
+    echo -e "</div>
+  <a class='carousel-control-prev' href='#carouselExampleControls' role='button' data-slide='prev'>
+    <span class='carousel-control-prev-icon' aria-hidden='true'></span>
+    <span class='sr-only'>Previous</span>
+  </a>
+  <a class='carousel-control-next' href='#carouselExampleControls' role='button' data-slide='next'>
+    <span class='carousel-control-next-icon' aria-hidden='true'></span>
+    <span class='sr-only'>Next</span>
+  </a>
+</div>" >> websitetest.html
+
+echo -e "
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src='https://code.jquery.com/jquery-3.2.1.slim.min.js' integrity='sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN' crossorigin='anonymous'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js' integrity='sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q' crossorigin='anonymous'></script>
+    <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js' integrity='sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl' crossorigin='anonymous'></script>
+  </body>
+</html>" >> websitetest.html
+
+
+
+
+
+firefox ./websitetest.html
 
 }
 
